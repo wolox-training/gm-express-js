2013c1abcba24c4edd1d7f780c5ba6be5cfdec53
Gmarn <marn.greg@gmail.com>
2018-03-20 11:44:24 -0300

    Set up sign in
    - Route + Middleware + Controller for signIn;
    - session manager in service to generate session token;
    - test + doc;

--- test/users.js
+++ test/users.js
@@ -4,6 +4,39 @@ const chai = require('chai'),
   expect = chai.expect;
 
 describe('users', () => {
+  describe('/users/sessions POST', () => {
+    it('expect to be successful', done => {
+      chai
+        .request(server)
+        .post('/users/sessions')
+        .send({ email: 'email1@wolox.com.ar', password: '12345678' })
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.have.property('token');
+          dictum.chai(res);
+        })
+        .then(() => done());
+    });
+
+    it('expect to return error for invalid email or/and password', done => {
+      chai
+        .request(server)
+        .post('/users/sessions')
+        .send({ email: 'invalid-email@wolox.com.ar', password: 'invalidPwd' })
+        .then(res => {
+          expect(res).to.not.have.status(200);
+          done();
+        })
+        .catch(err => {
+          expect(err).to.have.status(404);
+          expect(err.response).to.be.json;
+          expect(err.response.body).to.have.property('error');
+          done();
+        });
+    });
+  });
+
   describe('/users POST', () => {
     it('expect to be successful', done => {
       chai
